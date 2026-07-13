924cbe3fc89ca87455e2061efd24a21f05c9cc0d
NDMP <141399094+NDMP@users.noreply.github.com>
2026-07-14 00:05:31 +0530

    Add files via upload
    intial backendcreated

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+// Load environment variables
+dotenv.config();
+
+
+connectDB();
+
+// Create Express application
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Home Route
+app.get("/", (req, res) => {
+    res.send("ATS Backend Server is Running...");
+});
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
